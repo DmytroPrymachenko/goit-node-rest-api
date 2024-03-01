f9bd7d034d373c2a0dfb349761a24ef0e032c798
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-03-01 19:57:35 +0200

    fics

--- middlewares/isValididId.js
+++ middlewares/isValididId.js
@@ -1,9 +1,7 @@
-
 import { isValidObjectId } from "mongoose";
 import HttpError from "../helpers/HttpError.js";
 
 const isValidId = (req, res, next) => {
- 
   const { id } = req.params;
   if (!isValidObjectId(id)) {
     return next(HttpError(404, `${id} is not valid id`));
@@ -12,20 +10,3 @@ const isValidId = (req, res, next) => {
 };
 
 export default isValidId;
-
-const messageList = {
-  400: "Bad Request",
-  401: "Unauthorized",
-  403: "Forbidden",
-  404: "Not Found",
-  409: "Conflict",
-};
-
-const HttpError = (status, message = messageList[status]) => {
-  const error = new Error(message);
-  error.status = status;
-  return error;
-};
-
-export default HttpError;
-
